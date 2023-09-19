ead97d0b4341878f44841573557c15d951d7eb5c
Javiervazqz <javvazgar@alu.edu.gva.es>
2023-09-19 16:51:12 +0200

    Mi primer programa

---
+++ temp/HolaMundo/HolaMundo/Functions.cs
@@ -0,0 +1,13 @@
+﻿namespace HolaMundo
+{
+    internal class Functions
+    {
+        public static int GetMayor(int n1, int n2)
+        {
+            if (n1 > n2)
+                return n1;
+            return n2;
+
+        }
+    }
+}
---
+++ temp/HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HolaMundo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int r1;
+            r1 = Functions.GetMayor(1, 2);
+            Console.WriteLine(r1);
+            int r2;
+            r2 = Functions.GetMayor(10, -1);
+            System.Console.WriteLine(r2);
+        }
+    }
+}
