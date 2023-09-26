eda27e2d8e9ffbb2f2275c8d8ca7f4fe61a86deb
Javiervazqz <javvazgar@alu.edu.gva.es>
2023-09-26 16:59:47 +0200

    Commit Dolphin

--- temp/Objetos y todo eso/Objetos y todo eso/dolphin.cs
+++ temp/Objetos y todo eso/Objetos y todo eso/dolphin.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Objetos_y_todo_eso
 {
-    internal class dolphin
+    internal class Class1
     {
+        public class Dolphin
+        {
+            public double size;
+            public double life;
+            public string name;
+            public ColorType color;
+        }
+        public enum ColorType
+        {
+            blue,
+            white,
+            black
+        }
     }
 }
