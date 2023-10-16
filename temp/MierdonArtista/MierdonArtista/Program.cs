a12396a76e8ca2059cf66f8fcfb07c8806070b05
Javiervazqz <javvazgar@alu.edu.gva.es>
2023-10-16 16:25:27 +0200

    commit obj

---
+++ temp/MierdonArtista/MierdonArtista/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MierdonArtista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
