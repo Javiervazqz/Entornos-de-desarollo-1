ef39d41f96198b09a900cdf3bee6bb7e5c77aa28
Javiervazqz <javvazgar@alu.edu.gva.es>
2023-10-24 18:42:27 +0200

    commit classes

---
+++ PROG/EV1/Classes/Classes/Class.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Jar
+    {
+            double capacity = 1000.0;
+            double quantity = 200.0;
+            public void SetQuantity(double value)
+            {
+                quantity = value;
+            }
+            public double GetQuantity()
+            {
+                return quantity;
+            }
+            public void SetCapacity(double value)
+            {
+                capacity = value;
+            }
+            public double GetCapacity()
+            {
+                return capacity;
+            }
+            public double GetPercent()
+            {
+                return (quantity / capacity) * 100;
+            }
+            public double GetRemain()
+            {
+                return quantity - capacity;
+            }
+            public double AddQuanity(double value)
+            {
+                return quantity + value;
+            }
+    }
+}
---
+++ PROG/EV1/Classes/Classes/Classes.cs
@@ -0,0 +1,12 @@
+﻿namespace Classes
+{
+    internal class Classes
+    {
+        static void Main(string[] args)
+        {
+            Classes.Jar.SetQuantity(300.0);
+            Jar.SetCapacity(500.0);
+
+        }
+    }
+}
