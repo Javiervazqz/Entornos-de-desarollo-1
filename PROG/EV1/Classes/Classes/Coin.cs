89c50fef9e0a1031bc25f465f4b74829bddaed16
Javiervazqz <javvazgar@alu.edu.gva.es>
2023-10-30 15:10:10 +0100

    commit coinhecho

--- PROG/EV1/Classes/Classes/Coin.cs
+++ PROG/EV1/Classes/Classes/Coin.cs
@@ -8,40 +8,44 @@ namespace Classes
 {
     public enum Moneda
     {
-        E_100,
-        E_50,
-        E_20,
-        E_10,
-        E_5,
+        E_100 = 10000,
+        E_50 = 5000,
+        E_20 = 2000,
+        E_10 = 1000,
+        E_5 = 500,
+        E_001 = 1
     }
     public class CoinChange
     {
-        private Moneda coin;
-        public int ToNumber()
+        public static int ToNumber(Moneda coin)
         {
-            coin = Moneda.E_100;
-            if (coin == Moneda.E_100)
-            {
-                return 100 * 100;
-            }
-            if (coin == Moneda.E_50)
-            {
-                return 50 * 100;
-            }
-            if (coin == Moneda.E_20)
-            {
-                return 20 * 100;
-            }
-            if (coin == Moneda.E_10)
-            {
-                return 10 * 100;
-            }
-            else
-                return 5 * 100; 
+            return (int)coin;
+            //coin = Moneda.E_100;
+            //if (coin == Moneda.E_100)
+            //{
+            //    return 100 * 100;
+            //}
+            //if (coin == Moneda.E_50)
+            //{
+            //    return 50 * 100;
+            //}
+            //if (coin == Moneda.E_20)
+            //{
+            //    return 20 * 100;
+            //}
+            //if (coin == Moneda.E_10)
+            //{
+            //    return 10 * 100;
+            //}
+            //if (coin == Moneda.E_5)
+            //    return 5 * 100;
+            //else
+            //    return 001;
         }
         public Moneda ToMoneda(int centims)
         {
-
+            return (Moneda)centims;
         }
+
     }
 }
