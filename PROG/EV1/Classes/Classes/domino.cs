65e54f9727be6f86392f85808ae7a6ad76d6ebb0
Javiervazqz <javvazgar@alu.edu.gva.es>
2023-10-30 15:30:28 +0100

    commit domino

---
+++ PROG/EV1/Classes/Classes/domino.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class domino
+    {
+        public class dPiece
+        {
+            private int _value1;
+            private int _value2;
+            public int GetValue1()
+            {
+
+                return _value1;
+            }
+            public int GetValue2()
+            {
+                return _value2;
+            }
+        }
+        public class dDeck
+        {
+
+        }
+    }
+}
