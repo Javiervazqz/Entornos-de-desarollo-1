29bfe259d6024ccf0962605057c149aa3ceb09e3
Javiervazqz <javvazgar@alu.edu.gva.es>
2023-10-30 16:35:53 +0100

    domino montao

---
+++ PROG/EV1/Classes/Classes/DominoDeck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class DominoDeck
+    {
+        public class dDeck
+        {
+            public DominoPiece.Piece ExtractPiece(List<DominoPiece.Piece> l)
+            {
+                int r = utils.GetRandom(0, l.Count - 1);
+                DominoPiece.Piece p = l[r];
+                l.RemoveAt(r);
+                return p;
+            }
+            public int GetPieceCount(List<DominoPiece.Piece> l)
+            {
+                return l.Count;
+            }
+            public void AddPiece(DominoPiece.Piece p, List<DominoPiece.Piece> l)
+            {
+                l.Add(p);
+            }
+            public DominoPiece.Piece GetPieceAt(int value, List<DominoPiece.Piece> l)
+            {
+                DominoPiece.Piece p = l[value];
+                return p;
+            }
+            public List<DominoPiece.Piece> Shuffle(List<DominoPiece.Piece> l)
+            {
+                for (int i = 0; i <= l.Count - 1; i++)
+                {
+                    
+                }
+            }
+        }
+    }
+}
---
+++ PROG/EV1/Classes/Classes/DominoPiece.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class DominoPiece
+    {
+        public class Piece
+        {
+            private int _value1;
+            private int _value2;
+            List<Piece> l = new List<Piece>();
+            public int GetValue1()
+            {
+
+                return _value1;
+            }
+            public int GetValue2()
+            {
+                return _value2;
+            }
+            public int GetTotalValue()
+            {
+                return _value1 + _value2;
+            }
+        }
+    }
+}
