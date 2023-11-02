5f9a82973c86ab8fc0479d060c462a7d50e1bb7e
Javiervazqz <javvazgar@alu.edu.gva.es>
2023-11-02 15:54:51 +0100

    poker arreglao

--- PROG/EV1/Classes/Classes/Card.cs
+++ PROG/EV1/Classes/Classes/Card.cs
@@ -2,58 +2,66 @@
 
 namespace Classes
 {
+    public enum palo
+    {
+        Diamantes,
+        Picas,
+        Corazones,
+        Treboles
+    }
+    public enum figureType
+    {
+        A = 1,
+        J = 11,
+        Q = 12,
+        K = 13,
+        JOKER = 14
+    }
+    public enum colorType
+    {
+        Black = 0,
+        Red = 1,
+        UNKNOWN
+    }
     public class Card
     {
-        _palo palo;
-        _ColorType color;
-        int n = 0;
-        _figureType figure;
-        enum _palo
-        {
-            diamantes,
-            picas,
-            corazones,
-            treboles
-        }
-        enum _figureType
-        {
-            A = 1,
-            J = 11,
-            Q = 12,
-            K = 13,
-            JOKER = 14
-        }
-        enum _ColorType
-        {
-            Black = 0,
-            Red = 1
-        }
+        public palo p;
+        public colorType color;
+        public int n = 1;
+        public figureType figure;
         public static int GetNumber(int n)
         {
             return n;
         }
-        public static _palo GetPalo(_palo p)
+        public palo GetPalo(palo p)
         {
             return p;
         }
-        public static bool IsValid(int n)
+        public static bool IsValid(int? n)
         {
-            if (n > 14 || n < 0 || n == null)
-                return false;
-            return true;
+            return (n > 14 || n <= 0 || n == null);
         }
         public static bool IsFigure(int n)
         {
-            if (n >= 10)
-                return true;
-            return false;
+            if (!IsValid(n))
+                return false;
+            return (n > 10 || n == 1);
         }
-        public _ColorType GetColor(_ColorType color)
+        public colorType GetColor(int color)
         {
             if (color == 1)
-                return _ColorType.Black;
-            return _ColorType.Red;
+                return colorType.Black;
+            return colorType.Red;
         }
-        public _figureType 
+        public figureType GetFigure(int figure)
+        {
+            if (figure == 11)
+                return figureType.J;
+            if (figure == 12)
+                return figureType.Q;
+            if (figure == 13)
+                return figureType.K;
+            return figureType.JOKER;
+        }
     }
 }
