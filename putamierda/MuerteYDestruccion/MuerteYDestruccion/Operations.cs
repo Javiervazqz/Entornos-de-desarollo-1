97fdf1ac9889b70021a6fda8a13349d43164cf51
Javiervazqz <javvazgar@alu.edu.gva.es>
2023-11-22 18:35:41 +0100

    mierdon8

--- putamierda/MuerteYDestruccion/MuerteYDestruccion/Operations.cs
+++ putamierda/MuerteYDestruccion/MuerteYDestruccion/Operations.cs
@@ -18,38 +18,53 @@ namespace MuerteYDestruccion
                 return number2;
             return number1;
         }
-        public static List<int> Suma(List <int> l1, List <int> l2)
+        //public static List<int> Suma(List <int> l1, List <int> l2)
+        //{
+        //    l1 = GetBigger(l1, l2);
+        //    l2 = GetMinor(l1, l2);
+        //    List<int> result = new List<int>();
+        //    result = l1;
+        //    for (int i = l1.Count - 1; i >= 0; i--)
+        //    {
+        //        for (int j = l2.Count - 1; j >= 0; j--)
+        //        {
+        //            for (int k = result.Count - 1; k >= 0; k--)
+        //            {
+        //                int sum = l1[i] + l1[j];
+        //                if (sum >= 10)
+        //                {
+        //                    if (k == 0)
+        //                        return result;
+        //                    int coc = sum / 10;
+        //                    int resto = sum % 10;
+        //                    if (k < 0)
+        //                    {
+        //                        result.Add(coc);
+        //                        return result;
+        //                    }
+        //                    result[k] = resto;
+        //                    result[k - 1] = result[k] + coc;
+        //                }
+        //                result[k] = sum;
+        //            }
+        //        }
+        //    }
+        //    return result;
+        //}
+        public static List<int> Suma(List<int> l1, List<int> l2)
         {
-            l1 = GetBigger(l1, l2);
-            l2 = GetMinor(l1, l2);
             List<int> result = new List<int>();
-            result = l1;
-            for (int i = l1.Count - 1; i >= 0; i--)
+            int n1 = 0;
+            int n2 = 0;
+            for (int i = 0; i < l1.Count; i++)
             {
-                for (int j = l2.Count - 1; j >= 0; j--)
-                {
-                    for (int k = result.Count - 1; k >= 0; k--)
-                    {
-                        int sum = l1[i] + l1[j];
-                        if (sum >= 10)
-                        {
-                            if (k == 0)
-                                return result;
-                            int coc = sum / 10;
-                            int resto = sum % 10;
-                            if (k < 0)
-                            {
-                                result.Add(coc);
-                                return result;
-                            }
-                            result[k] = resto;
-                            result[k - 1] = result[k] + coc;
-                        }
-                        result[k] = sum;
-                    }
-                }
+
             }
-            return result;
+            for (int j = 0; j < l2.Count; j++)
+            {
+
+            }
+
         }
     }
 }
--- putamierda/MuerteYDestruccion/MuerteYDestruccion/Program.cs
+++ putamierda/MuerteYDestruccion/MuerteYDestruccion/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            List<int> list = utils.ConvertToList(16);
-            List<int> list2 = utils.ConvertToList(16);
+            List<int> list = utils.ConvertToList(12);
+            List<int> list2 = utils.ConvertToList(12);
             List<int> list3 = Operations.Suma(list, list2);
             for (int i = 0; i < list3.Count; i++)
             {
