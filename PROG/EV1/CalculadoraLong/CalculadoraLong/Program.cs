0f69b3ec4e2817b7a5e85b84ae70b4724a7d301f
Javiervazqz <javvazgar@alu.edu.gva.es>
2023-12-07 15:01:40 +0100

    commit practicas

--- PROG/EV1/CalculadoraLong/CalculadoraLong/Program.cs
+++ PROG/EV1/CalculadoraLong/CalculadoraLong/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace MuerteYDestruccion
+﻿namespace CalculadoraLong
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            List<int> list = utils.ConvertToList(12);
-            List<int> list2 = utils.ConvertToList(12);
+            long a = 12;
+            long b = 14;
+            List<int> list = utils.ConvertToList(a);
+            List<int> list2 = utils.ConvertToList(b);
             List<int> list3 = Operations.Suma(list, list2);
             for (int i = 0; i < list3.Count; i++)
             {
