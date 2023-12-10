452d3842217ce2cca8329f7ae94a3e26a4b4c7b3
Javier Marín <56446732+jmarinprofe@users.noreply.github.com>
2023-12-10 07:23:35 +0100

    Update Class.cs

--- PROG/EV1/Classes/Classes/Class.cs
+++ PROG/EV1/Classes/Classes/Class.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
+    // Javi: los métodos de esta clase no son exáctamente los que se pedían
     public class Jar
     {
             double capacity = 1000.0;
