08ba17c52d88e8c60076215f3f9dd5eb32eda892
Javiervazqz <javvazgar@alu.edu.gva.es>
2024-01-09 19:51:25 +0100

    commit queue

--- PROG/EV2/DAMLibTest/DAMLibTest/Program.cs
+++ PROG/EV2/DAMLibTest/DAMLibTest/Program.cs
@@ -6,13 +6,19 @@ namespace DamLibTest
     {
         static void Main(string[] args)
         {
-            DamLib.Stack<string> s = new DamLib.Stack<string>();
-            s.Push("Jose Mari");
-            s.Push("Jose Manuel");
-            s.Push("Josetrix");
-            s.Pop();
-            Console.WriteLine(s.IsEmpty());
-            s.PrintStack();
+            //DamLib.Stack<string> s = new DamLib.Stack<string>();
+            //s.Push("Jose Mari");
+            //s.Push("Jose Manuel");
+            //s.Push("Josetrix");
+            //s.Pop();
+            //Console.WriteLine(s.IsEmpty());
+            //s.PrintStack();
+            DamLib.Queue<string> q = new DamLib.Queue<string>();
+            q.ToQueue("Jose Mari");
+            q.ToQueue("Jose Manuel");
+            q.ToQueue("Josetrix");
+            q.DeQueue();
+            q.PrintQueue();
         }
     }
 }
---
+++ PROG/EV2/DAMLibTest/DamLib/Queue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DamLib
+{
+    public class Queue<T>
+    {
+        T[] _queue = new T[1];
+        public int Count => _queue.Length;
+        public T First => _queue[1];
+        public T Last => _queue[_queue.Length - 1];
+        public bool Empty => _queue.Length == 0;
+        public void ToQueue (T newelement)
+        {
+            if (newelement == null)
+                return;
+            T[] _queue = new T[Count + 1];
+            for (int i = 0; i <= this._queue.Length - 1; i++)
+            {
+                _queue[i] = this._queue[i];
+                if (i == this._queue.Length - 1)
+                    _queue[i + 1] = newelement;
+            }
+             this._queue = _queue;
+        }
+        public void DeQueue()
+        {
+            T[] _queue = new T[Count - 1];
+            for (int i = _queue.Length - 1; i >= 0; i--)
+            {
+                _queue[i] = this._queue[i];
+                if (i == 0)
+                    _queue[0] = default(T);
+            }
+            this._queue = _queue;
+        }
+        public void PrintQueue()
+        {
+            for (int i = 0; i < _queue.Length; i++)
+            {
+                Console.WriteLine(_queue[i]);
+            }
+        }
+        public void Clear()
+        {
+            T[] _voidarray = new T[1];
+            _queue = _voidarray;
+        }
+        public void PrintFirst()
+        {
+            Console.WriteLine(First);
+        }
+        public void PrintLast()
+        {
+            Console.WriteLine(Last);
+        }
+    }
+}
