7d9692778eef74ee2feb55bc0a0b196529fca404
Javiervazqz <javvazgar@alu.edu.gva.es>
2024-01-17 12:55:21 +0100

    practica sort array

--- temp/PruebaSortArray/PruebaSortArray/Array.cs
+++ temp/PruebaSortArray/PruebaSortArray/Array.cs
@@ -1,26 +1,21 @@
-﻿using Microsoft.Office.Interop.Outlook;
-using System;
+﻿using System;
 
 namespace PruebaSortArray
 {
     public class Array
     {
-        int[] _array = new int[5];
-        _array[0] = 2;
-        _array[1] = 5;
-        _items[2] = 0;
-        public void Sort()
+        public static void Sort(int[] array)
         {
-            for (int i = 1, j = 0; i < _items.Length; i++, j++)
+            for (int i = 1, j = 0; i < array.Length; i++, j++)
             {
                 int AuxHash = 0;
-                int v1 = _items[i - 1];
-                int v2 = _items[i];
+                int v1 = array[i - 1];
+                int v2 = array[i];
                 if (v1 >= v2)
                 {
                     AuxHash = v1;
                     v1 = v2;
-                    v2 = _items[i + 1];
+                    v2 = array[i + 1];
                 }
             }
         }
--- temp/PruebaSortArray/PruebaSortArray/Program.cs
+++ temp/PruebaSortArray/PruebaSortArray/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-
+            int[] array = new int[5] { 2, 3, 5, 1, 8 };
+            Array.Sort(array);
         }
     }
 }
