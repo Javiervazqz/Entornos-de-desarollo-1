c571a134f400598e8b959c491c8e423d7a0ef647
Javiervazqz <javvazgar@alu.edu.gva.es>
2024-01-18 16:50:42 +0100

    ref out

---
+++ temp/SegundineGrado/SegundineGrado/EcuacionSegundoGrado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SegundineGrado
+{
+    public class EcuacionSegundoGrado
+    {
+        public static (double res1, double res2) EcuacionDoble(ref double a, ref double b, ref double c)
+        {
+            if (a == 0)
+                return (double.NaN, double.NaN);
+            double r = b * b - 4 * a * c;
+            if (r < 0)
+                return (double.NaN, double.NaN);
+            double root = Math.Sqrt(r);
+            double denom = 1.0 / (2.0 * a);
+            double res1 = (-b + root) * denom;
+            double res2 = (-b - root) * denom;
+            return (res1, res2);
+        }
+        public static bool EcuacionDobleOut(double a, double b, double c, out double res1, out double res2)
+        {
+            res1 = double.NaN;
+            res2 = double.NaN;
+            if (a == 0.0)
+                return false;
+            double r = b * b - 4 * a * c;
+            if (r < 0)
+                return false;
+            double root = Math.Sqrt(r);
+            double denom = 1.0 / (2.0 * a);
+            res1 = (-b + root) * denom;
+            res2 = (-b - root) * denom;
+            return true;
+        }
+    }
+}
---
+++ temp/SegundineGrado/SegundineGrado/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SegundineGrado
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0.5, b = 2.5, c = 0.2;
+            Console.WriteLine(EcuacionSegundoGrado.EcuacionDoble(ref a, ref b, ref c));
+            Console.WriteLine(EcuacionSegundoGrado.EcuacionDobleOut(a, b, c, out double res1, out double res2));
+        }
+    }
+}
