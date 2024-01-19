ed454d20d948654375a615432c8d413ce2ada7e5
Javiervazqz <javvazgar@alu.edu.gva.es>
2024-01-19 18:20:34 +0100

    robertikoprospectodelosastosdeHouston

--- PROG/EV2/DAMLibTest/DAMLibTest/SetTest.cs
+++ PROG/EV2/DAMLibTest/DAMLibTest/SetTest.cs
@@ -2,16 +2,25 @@
 
 namespace DamLibTest
 {
+    public class ISet<T>
+    {
+        bool IsEmpty => 
+    }
+
     public class SetTest
     {
         public static void Test1(DamLib.Set<string> set)
         {
 
         }
+        public static void Test2(DamLib.ItemSet<string> set)
+        {
+
+        }
         static void Main(string[] args)
         {
             DamLib.Set<string> st1 = new DamLib.Set<string>();
-            DamLib.Set<string> st2 = new DamLib.Set<string>();
+            DamLib.ItemSet<string> st2 = new DamLib.ItemSet<string>();
         }
     }
 }
