d0674cb2ae3d25bf8a9cbf9eaeb3a6da74d6ee69
Javiervazqz <javvazgar@alu.edu.gva.es>
2024-01-22 19:53:58 +0100

    dictionary sin terminar

--- PROG/EV2/DAMLibTest/DamLib/Dictionary.cs
+++ PROG/EV2/DAMLibTest/DamLib/Dictionary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace DamLib
 {
@@ -12,6 +13,10 @@ namespace DamLib
         Item[] _items = new Item[0];
         public bool Empty => Count < 0;
         public int Count => _items.Length;
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
         public void AddOrReplace(K kons, V val)
         {
             if (Empty)
@@ -31,6 +36,35 @@ namespace DamLib
             }
             _tempArray[Count] = _items[Count - 1];
             _items = _tempArray;
+        }
+        public void RemoveAll(K kons)
+        {
+            if (Empty)
+                return;
+            int j = 0;
+            Item[] _tempArray = new Item[Count - 1];
+            for (int i = 0; i < Count - 1; i++)
+            {
+                if (_items[i].key.Equals(kons))
+                {
+                    j++;
+                }
+                _items[i] = _tempArray[j];
+            }
+        }
+        public void RemoveOne(K kons)
+        {
+            if (Empty)
+                return;
+            Item[] _tempArray = new Item[Count - 1];
+            for (int i = 0; i < Count - 1; i++)
+            {
+                if (_items[i].key.Equals(kons))
+                {
+                    break;
+                }
+                _items[i] = _tempArray[i];
+            }
         }
     }
 }
