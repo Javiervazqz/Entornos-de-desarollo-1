8f2c698d92820a7d7c8092503d5b9dd684b8b8ad
Javiervazqz <145146511+Javiervazqz@users.noreply.github.com>
2024-02-01 16:06:09 +0100

    commit nodos rancios

---
+++ PROG/EV2/NodosArbol/NodosArbol/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NodosArbol
+{
+    public class Node<T>
+    {
+        Node<T> _parent;
+        public int GetLevel()
+        {
+            if (this == null)
+                return -1;
+            return _parent.GetLevel() + 1;
+        }
+        public Node<T> GetRoot()
+        {
+
+        }
+    }
+}
---
+++ PROG/EV2/NodosArbol/NodosArbol/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NodosArbol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
