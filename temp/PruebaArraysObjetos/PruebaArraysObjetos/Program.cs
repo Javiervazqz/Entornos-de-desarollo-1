173db52082905b44a71948b2bbb9bef26b7e23de
Javiervazqz <javvazgar@alu.edu.gva.es>
2024-02-27 11:47:02 +0100

    practicas

--- temp/PruebaArraysObjetos/PruebaArraysObjetos/Program.cs
+++ temp/PruebaArraysObjetos/PruebaArraysObjetos/Program.cs
@@ -5,18 +5,36 @@
         static void Main(string[] args)
         {
             Perro[] arrayPerros = new Perro[3];
-            arrayPerros[0] = new Perro();
-            arrayPerros[1] = new Perro();
-            arrayPerros[2] = new Perro();
+            arrayPerros[0] = new Perro("juan");
+            arrayPerros[1] = new Dalmata("juanito");
+            arrayPerros[2] = new Perro("juan");
             arrayPerros[0].Ladrar();
             arrayPerros[1].Ladrar();
             arrayPerros[2].Ladrar();
         }
         public class Perro
         {
-            public void Ladrar()
+            protected string name; 
+            public Perro(string name)
             {
-                Console.WriteLine("Guau");
+                this.name = name;
+            }
+            public virtual void Ladrar()
+            {
+                
+                Console.WriteLine($"Guau, soy {name}");
+            }
+        }
+        public class Dalmata : Perro
+        {
+            public Dalmata(string name) : base(name)
+            {
+
+            }
+            public override void Ladrar()
+            {
+                base.Ladrar();
+                Console.WriteLine($"Auuu, soy {name}");
             }
         }
     }
