48f58df8a9763b0c71e788846af218f47450f6e5
Javiervazqz <javvazgar@alu.edu.gva.es>
2024-02-27 11:25:16 +0100

    array objetos

---
+++ temp/PruebaArraysObjetos/PruebaArraysObjetos/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PruebaArraysObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Perro[] arrayPerros = new Perro[3];
+            arrayPerros[0] = new Perro();
+            arrayPerros[1] = new Perro();
+            arrayPerros[2] = new Perro();
+            arrayPerros[0].Ladrar();
+            arrayPerros[1].Ladrar();
+            arrayPerros[2].Ladrar();
+        }
+        public class Perro
+        {
+            public void Ladrar()
+            {
+                Console.WriteLine("Guau");
+            }
+        }
+    }
+}
