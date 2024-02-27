d1ac149dd9541441153f87aca0755a7743c15454
Javiervazqz <145146511+Javiervazqz@users.noreply.github.com>
2024-02-27 16:53:31 +0100

    prueba delegados 3

---
+++ temp/PruebaDelegados3/PruebaDelegados3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PruebaDelegados3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 5;
+            delegateCheck check = new delegateCheck(delegate (int v) {
+                return v >= 5; // For example, checking if the value is greater than 5
+            });
+
+            bool result = Filter(valor, check);
+            Console.WriteLine(result);
+        }
+
+        public delegate bool delegateCheck(int v);
+
+        public static bool Filter(int value, delegateCheck checker)
+        {
+            return checker(value);
+        }
+    }
+}
