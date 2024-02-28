32fe21af0e4508aab6c01e32197b5431891ba0b7
Javiervazqz <javvazgar@alu.edu.gva.es>
2024-02-28 12:20:43 +0100

    PseudoRandom

---
+++ temp/PseudoRandom/PseudoRandom/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PseudoRandom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PseudoRandom.Get0to999());
+        }
+    }
+    public static class PseudoRandom
+    {
+        public static int Get0to999()
+        {
+            return DateTime.Now.Millisecond;
+        }
+        //public static override string ToString()
+        //{
+        //    return $"El numero es: {Get0to999()}";
+        //}
+    }
+}
