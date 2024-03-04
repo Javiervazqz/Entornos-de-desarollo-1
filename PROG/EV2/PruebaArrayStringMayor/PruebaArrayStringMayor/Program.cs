9a3cb0e5bf21ebaf72ab7f7deda41c841cac9949
Javiervazqz <145146511+Javiervazqz@users.noreply.github.com>
2024-03-04 20:42:12 +0100

    asd

---
+++ PROG/EV2/PruebaArrayStringMayor/PruebaArrayStringMayor/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PruebaArrayStringMayor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = { "hola", "adios", "vengahastaluego", "yeybuenosdiasjhajkhasdf" };
+            Empanao Roberto = new Empanao();
+            Console.WriteLine(Roberto.GetGreater(strings));
+        }
+        public class Empanao
+        {
+            public string GetGreater(string[] strings)
+            {
+                int Count = strings.Length;
+                string mayor;
+                int lenM = 0;
+                for (int i = 0; i < Count; i++)
+                {
+                    int len = 0;
+                    string may;
+                    foreach(char n in strings[i])
+                    {
+                        len++;
+                    }
+                    if (lenM > len)
+                    {
+                        may = strings[i];
+                    }
+                    lenM = len;
+                    mayor = may;
+                }
+                return mayor;
+            }
+        }
+    }
+}
