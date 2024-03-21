71b152991b58188c122cb9c74f78bae81d2f22a8
Javiervazqz <145146511+Javiervazqz@users.noreply.github.com>
2024-03-21 16:52:09 +0100

    pruebadispo

---
+++ temp/PruebaDisposCritika/PruebaDisposCritika/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PruebaDisposCritika
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Prueba p = CrearPrueba())
+            {
+                // if (bicho != null)
+                // bicho.Dispose();
+                //p.SayHello();
+            }
+            //p.Dispose();
+        }
+        public class Prueba : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("Dispose");
+            }
+
+            public void SayHello()
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+        public static Prueba CrearPrueba()
+        {
+            return null;//new Prueba();
+        }
+    }
+}
