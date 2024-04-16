c1ba8104734e335edee0ae629e71668c38ef340c
Javiervazqz <145146511+Javiervazqz@users.noreply.github.com>
2024-04-16 18:06:46 +0200

    kj

---
+++ temp/EntornosRarisimo/EntornosRarisimo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EntornosRarisimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 2;
+            int s = 0;
+            int n;
+
+            n = Int32.Parse(Console.ReadLine());
+            while (j <= n / 2) //Bucle infinito porque hay numeros que no cumplen la condicion del while y del if
+            {
+                if (n % j == 0)
+                {
+                    s = s + 1;
+                    break;
+                }
+                j = j + 1;
+            }
+            if (s == 0 && n > 1)
+                Console.Write(n + " es primo");
+            else
+                Console.Write(n + " no es primo");
+        }
+    }
+}
