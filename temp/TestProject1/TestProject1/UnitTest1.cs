9adc6b8296d093ab614be5b04602c5fffb6b509e
Javiervazqz <145146511+Javiervazqz@users.noreply.github.com>
2024-05-07 19:57:47 +0200

    yeaouyea

---
+++ temp/TestProject1/TestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+
+namespace TestProject1
+{
+    [TestFixture] public class TestAlumno
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
