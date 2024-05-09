012a1cb348972b5e3ae788d413fc01e5529f1d12
Javiervazqz <145146511+Javiervazqz@users.noreply.github.com>
2024-05-09 16:34:30 +0200

    nig

---
+++ temp/WpfStudents/Database/DatabaseRAM.cs
@@ -0,0 +1,52 @@
+﻿namespace Database
+{
+    public class DatabaseRAM : IDatabase
+    {
+        List<Student> _students = new List<Student>();
+        public int Count => _students.Count;
+        public long AddStudent(Student student)
+        {
+            if (student == null)
+                return -1; // Id cannot be less than zero
+            _students.Add(student);
+            return student.Id;
+        }
+
+        public Student GetStudentAt(int index)
+        {
+            try
+            {
+                if (index < 0 || index == null)
+                    throw new ArgumentOutOfRangeException("index");
+                return _students[index];
+            }
+            catch(Exception ex) 
+            {
+                
+            }
+        }
+
+        public Student GetStudentWithId(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            RemoveStudentWithId(student.Id);
+        }
+
+        public void RemoveStudentWithId(long id)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+
+            }
+        }
+
+        public void UpdateStudent(long id, Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ temp/WpfStudents/Database/IDatabase.cs
@@ -0,0 +1,12 @@
+﻿namespace Database
+{
+    public interface IDatabase
+    {
+        public long AddStudent(Student student);
+        public Student GetStudentWithId(long id);
+        public Student GetStudentAt(int index);
+        public void UpdateStudent(long id, Student student);
+        public void RemoveStudent(Student student);
+        public void RemoveStudentWithId(long id);
+    }
+}
