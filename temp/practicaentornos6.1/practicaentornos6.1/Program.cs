b79d0e57836c0a6a18454bec8b05476fe714434e
Javiervazqz <145146511+Javiervazqz@users.noreply.github.com>
2024-05-27 18:56:23 +0200

    compila huevon

---
+++ temp/practicaentornos6.1/practicaentornos6.1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int funcion(int a, int b)
+        {
+            int resultado;
+            if (a < b)
+                resultado = a;
+            else
+                resultado = funcion(a - b, b);
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            funcion(5, 2);
+        }
+    }
+}
+
