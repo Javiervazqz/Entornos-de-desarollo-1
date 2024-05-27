35300f36ac29ad8b78e1581e3152e8645f4aa2dd
Javiervazqz <145146511+Javiervazqz@users.noreply.github.com>
2024-05-27 17:28:45 +0200

    walapop

--- PROG/EV3/Wallapop/ApplicationModel/Product.cs
+++ PROG/EV3/Wallapop/ApplicationModel/Product.cs
@@ -6,44 +6,9 @@ namespace ApplicationModel
 {
     public class Product
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public double Price { get; set; }
         public string ImagePath { get; set; }
+        public double Price { get; set; }
     }
-
-    //public class DataAccess
-    //{
-    //    private string connectionString = "Data Source=(WALLAPOP.dbo)\\MSSQLLocalDB;AttachDbFilename=|C:\\Users\\javvazgar\\Documents\\GitHub\\Entornos-de-desarollo-1\\PROG\\EV3\\Wallapop\\WALLAPOP.sql|\\MiBaseDeDatos.mdf;Integrated Security=True";
-
-    //    public List<Product> LoadProducts()
-    //    {
-    //        List<Product> products = new List<Product>();
-
-    //        using (var connection = new SqlConnection(connectionString))
-    //        {
-    //            string query = "SELECT Id, Name, Description, ImagePath FROM Productos";
-    //            SqlCommand command = new SqlCommand(query, connection);
-
-    //            connection.Open();
-    //            SqlDataReader reader = command.ExecuteReader();
-
-    //            while (reader.Read())
-    //            {
-    //                Product product = new Product
-    //                {
-    //                    Id = Convert.ToInt32(reader["Id"]),
-    //                    Name = reader["Name"].ToString(),
-    //                    Description = reader["Description"].ToString(),
-    //                    ImagePath = reader["ImagePath"].ToString()
-    //                    // Otras propiedades si es necesario
-    //                };
-    //                products.Add(product);
-    //            }
-    //        }
-
-    //        return products;
-    //    }
-    //}
 }
--- PROG/EV3/Wallapop/Wallapop/MainWindow.xaml.cs
+++ PROG/EV3/Wallapop/Wallapop/MainWindow.xaml.cs
@@ -4,39 +4,29 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Wallapop
 {
     public partial class MainWindow : Window
     {
-        public List<Product> Products { get; set; } = new List<Product>();
+        public ObservableCollection<Product> Products { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
 
-            Products.Add(new Product { Name = "Artículo 1", Description = "Descripción del artículo 1" });
-            Products.Add(new Product { Name = "Artículo 2", Description = "Descripción del artículo 2" });
-
+            Products = new ObservableCollection<Product>
+            {
+                new Product { Name = "Shrek", Description = "Peluche Shrek verde como un moco", ImagePath = "C:\\Users\\javvazgar\\Documents\\GitHub\\Entornos-de-desarollo-1\\PROG\\EV3\\Wallapop\\imagesshrek.jpeg", Price = 9999.99 },
+                new Product { Name = "Artículo 2", Description = "Descripción del artículo 2", ImagePath = "C:\\Users\\javvazgar\\Documents\\GitHub\\Entornos-de-desarollo-1\\PROG\\EV3\\Wallapop\\images\\image2.jpg", Price = 15.99 }
+            };
 
             DataContext = this;
-
         }
-        //private List<Product> LoadProducts()
-        //{
-            // Aquí cargarías los productos desde tu fuente de datos
-            // Por ejemplo, desde una base de datos o un archivo
-            // Devuelve una lista de productos con las rutas de imagen
-        //}
+
         private BitmapImage LoadImage(string imagePath)
         {
             BitmapImage image = new BitmapImage();
