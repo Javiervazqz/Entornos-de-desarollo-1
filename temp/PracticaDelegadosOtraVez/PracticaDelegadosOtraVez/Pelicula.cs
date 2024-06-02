ae0c9f7b9a4b0e1edfc02ccb30f89f1dee3ee825
Javiervazqz <javvazgar@alu.edu.gva.es>
2024-06-02 22:24:11 +0200

    cositas

---
+++ temp/PracticaDelegadosOtraVez/PracticaDelegadosOtraVez/Pelicula.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaDelegadosOtraVez
+{
+    public class Pelicula
+    {
+        public string title;
+        public string directorName;
+        public int yearReleased;
+        public Pelicula(string title, string directorName, int yearReleased)
+        {
+            this.title = title;
+            this.directorName = directorName; 
+            this.yearReleased = yearReleased;
+        }
+    }
+}
---
+++ temp/PracticaDelegadosOtraVez/PracticaDelegadosOtraVez/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace PracticaDelegadosOtraVez
+{
+    internal abstract class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> enteros = new() { 1,2,3,4,5,6,7,8,9,10 };
+            //DelegadoEnteros delegadoint = (value) => Console.WriteLine(--value);
+            //delegadoint(enteros[0]);
+            //MiDelegado delegado = (value) => Console.WriteLine($"hola {value}");
+            //delegado("javi");
+            //Visitor filtro = (value) => value % 2 != 0;
+            //foreach (int n in enteros)
+            //{
+            //    if (filtro(n))
+            //        Console.WriteLine(n);
+            //}
+            //int valor1 = 5;
+            //int valor2 = 3;
+            //Calculador suma = (valor1, valor2) => valor1 + valor2;
+            //Calculador resta = (valor1, valor2) => valor1 - valor2;
+            //Calculador multiplicacion = (valor1, valor2) => valor1 * valor2;
+            //Calculador division = (valor1, valor2) => valor1 / valor2;
+            //Calcular(valor1, valor2, division);
+            ProcesadorCadena mayus = (cadena) => cadena.ToUpper();
+            ProcesadorCadena minus = (cadena) => cadena.ToLower();
+            ProcesadorCadena 
+        }
+        public delegate void MiDelegado(string value);
+        public delegate void DelegadoEnteros(int value);
+        public delegate bool Visitor(int value);
+        public delegate double Calculador(int value1, int value2);
+        public delegate string ProcesadorCadena(string cadena);
+        public static void Calcular(int v1, int v2, Calculador calculador)
+        {
+            Console.WriteLine(calculador(v1, v2));
+        }
+        public static void ImprimirString(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
